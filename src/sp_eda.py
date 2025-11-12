f23af872692bb2d25de776d67e9dc995f9144d56
inaki1987 <ignacio.fernandez.ordonez@outlook.com>
2025-11-12 06:26:03 +0100

    comienzo limpieza por columnas

--- src/sp_eda.py
+++ src/sp_eda.py
@@ -1,24 +1,25 @@
 
+from readline import redisplay
 import pandas as pd
 
 
 def eda_prelim(df):
     
-    display(df.sample(5))
+    redisplay(df.sample(5))
     print('----------')
     print('DIMENSIONES')
     print(f'El conjunto de datos presenta {df.shape[0]} filas y {df.shape[1]} columnas')
     print('----------')
     print('INFORMACION DE COLUMNAS')
-    display(df.info())
+    redisplay(df.info())
     print('----------')
     print('DUPLICADOS')
     print(f'Tenemos {df.duplicated().sum()} duplicados')
     print('----------')
     print('ELEMENTOS NULOS')
-    display(df.isnull().sum()/df.shape[0]*100)
+    redisplay(df.isnull().sum()/df.shape[0]*100)
     print('----------')
     print('FRECUENCIA CATEGORÍAS')
     for col in df.select_dtypes(include='object').columns:
-        print (df[col].value_counts())
+        print (df[col].value_counts(dropna = False))
         print('-----------')
